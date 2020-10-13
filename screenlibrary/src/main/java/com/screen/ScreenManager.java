aea84192e0eb0618985e7a56fb1db0a022329583
liuhuiliang <liuhuiliang@bianfeng.com>
2020-10-13 10:31:04 +0800

    添加底部返回键等虚拟按键显示隐藏

--- screenlibrary/src/main/java/com/screen/ScreenManager.java
+++ screenlibrary/src/main/java/com/screen/ScreenManager.java
@@ -320,6 +320,31 @@ public class ScreenManager extends FragmentManager.FragmentLifecycleCallbacks im
         }
     }
 
+    public static void hideSoftBack(Window window){
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) {
+            View v =window.getDecorView();
+            v.setSystemUiVisibility(View.GONE);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            View decorView = window.getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+    }
+
+    public static void showSoftBack(Window window){
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) {
+            //低版本sdk
+            View v = window.getDecorView();
+            v.setSystemUiVisibility(View.VISIBLE);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            View decorView =window.getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+    }
+
 
     @Override
     public boolean handleMessage(Message msg) {
